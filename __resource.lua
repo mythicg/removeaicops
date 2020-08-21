770781d0ff822236931764bc6a6ff4839b03dea0
Mythic <61439974+MythicGaming5900@users.noreply.github.com>
2020-08-20 21:26:02 -0600

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,6 @@
+----------------------------------------------
+--Script Made By Mythic aka Michael          |
+--Discord: https://discord.gg/bhwyhYk        |
+----------------------------------------------
+
+client_script "traffic.lua"
