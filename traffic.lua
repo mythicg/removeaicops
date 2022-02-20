924561f4e2dccc841ca8535aac6510806c4003c7
Mythic <61439974+MythicGaming5900@users.noreply.github.com>
2022-02-19 22:09:46 -0700

    Update traffic.lua

--- traffic.lua
+++ traffic.lua
@@ -1,6 +1,5 @@
 ----------------------------------------------
---Script Made By Mythic aka Michael          |
---Discord: https://discord.gg/bhwyhYk        |
+--Script Made By Mythic       |
 ----------------------------------------------
 
 
